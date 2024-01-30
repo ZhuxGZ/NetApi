7a00a40e0b41a95c9d35f14772cef53c59df1a57
IgnacioBergantinosGM2 <ignacio.bergantinos@gm2dev.com>
2024-01-30 11:23:01 -0300

    chore: init the git project

---
+++ NetApi/Controllers/PokemonController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetApi.Models;
+
+namespace NetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private static List<Pokemon> pokemons = new List<Pokemon>
+        {
+           new Pokemon
+           {
+              Id = 1,
+              Name = "Charmander",
+              Type = "Fire"
+           },
+           new Pokemon
+           {
+              Id = 2,
+              Name = "Bulbasaur",
+              Type = "Grass"
+           }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Pokemon>>> GetAllPokemons()
+        {
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pokemon>> GetPokemon(int id)
+        {
+            var pokemon = pokemons.Find(X => X.Id == id);
+
+            if (pokemon == null)
+            {
+                return NotFound("Sorry, but this pokemon doesn't exist");
+            }
+
+            return Ok(pokemon);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Pokemon>>> AddHero(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ NetApi/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NetApi.Models
+{
+	public class Pokemon
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = String.Empty;
+		public string Type { get; set; } = String.Empty;
+	}
+}
+
